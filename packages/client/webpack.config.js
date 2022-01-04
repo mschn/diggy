d1e9224aaa58b00a1cb6240e308661cb5c00b11a
Mathieu Schnoor <mathieu.schnoor@gmail.com>
2022-01-04 13:24:53 +0100

    chore: update webpack-cli

--- packages/client/webpack.config.js
+++ packages/client/webpack.config.js
@@ -42,7 +42,7 @@ module.exports = {
     clean: true
   },
   devServer: {
-    contentBase: path.join(__dirname, 'dist'),
+    static: path.join(__dirname, 'dist'),
     compress: true,
     port: 9000
   },
